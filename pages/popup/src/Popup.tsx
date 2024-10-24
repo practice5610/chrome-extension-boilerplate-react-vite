1db18edb857e738b5c8e5ce340c27de860f121f6
sami khan <109520665+practice5610@users.noreply.github.com>
2024-10-24 15:14:09 +0500

    wip

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -13,35 +13,39 @@ const notificationOptions = {
 const Popup = () => {
   const theme = useStorage(exampleThemeStorage);
   const isLight = theme === 'light';
+  const logo = isLight ? 'popup/logo_vertical.svg' : 'popup/logo_vertical_dark.svg';
+  const goGithubSite = () =>
+    chrome.tabs.create({ url: 'https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite' });
 
   const injectContentScript = async () => {
     const [tab] = await chrome.tabs.query({ currentWindow: true, active: true });
 
     if (tab.url!.startsWith('about:') || tab.url!.startsWith('chrome:')) {
       chrome.notifications.create('inject-error', notificationOptions);
-      return; // Stop further execution
     }
 
-    try {
-      await chrome.scripting.executeScript({
+    await chrome.scripting
+      .executeScript({
         target: { tabId: tab.id! },
         files: ['/content-runtime/index.iife.js'],
+      })
+      .catch(err => {
+        // Handling errors related to other paths
+        if (err.message.includes('Cannot access a chrome:// URL')) {
+          chrome.notifications.create('inject-error', notificationOptions);
+        }
       });
-
-      // Send a message to the content script after injection
-      chrome.tabs.sendMessage(tab.id!, { greeting: 'Hello from popup' }, response => {
-        console.log('Response from content script:', response.response);
-      });
-    } catch (err) {
-      if (err.message.includes('Cannot access a chrome:// URL')) {
-        chrome.notifications.create('inject-error', notificationOptions);
-      }
-    }
   };
 
   return (
     <div className={`App ${isLight ? 'bg-slate-50' : 'bg-gray-800'}`}>
       <header className={`App-header ${isLight ? 'text-gray-900' : 'text-gray-100'}`}>
+        <button onClick={goGithubSite}>
+          <img src={chrome.runtime.getURL(logo)} className="App-logo" alt="logo" />
+        </button>
+        <p>
+          Edit <code>pages/popup/src/Popup.tsx</code>
+        </p>
         <button
           className={
             'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' +
@@ -50,8 +54,25 @@ const Popup = () => {
           onClick={injectContentScript}>
           Click to inject Content Script
         </button>
+        <ToggleButton>Toggle theme</ToggleButton>
       </header>
     </div>
+  );
+};
+
+const ToggleButton = (props: ComponentPropsWithoutRef<'button'>) => {
+  const theme = useStorage(exampleThemeStorage);
+  return (
+    <button
+      className={
+        props.className +
+        ' ' +
+        'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' +
+        (theme === 'light' ? 'bg-white text-black shadow-black' : 'bg-black text-white')
+      }
+      onClick={exampleThemeStorage.toggle}>
+      {props.children}
+    </button>
   );
 };
 
